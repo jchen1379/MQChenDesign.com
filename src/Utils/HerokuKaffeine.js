9f100c01691d50cd3d8d5dff36772f69a348b227
jchen1379 <jianhuichen1379@gmail.com>
2021-09-08 17:11:48 -0400

    remove HerokuCaffeine

--- src/Utils/HerokuKaffeine.js
+++
@@ -1,8 +0,0 @@
-let request = new XMLHttpRequest();
-
-const WakeUpHeroku = () => {
-  request.open("GET", "https://mq-chen-design-react.herokuapp.com/");
-  request.send();
-}
-
-setInterval(WakeUpHeroku, 15*60*1000);
