be64f729e23cea6b2bbc302d2443c7189aaf2159
jchen1379 <jianhuichen1379@gmail.com>
2021-09-08 09:25:28 -0400

    testing herokuKaffeine

--- public/HerokuKaffeine.js
+++ public/HerokuKaffeine.js
@@ -2,8 +2,8 @@ let request = new XMLHttpRequest();
 
 const WakeUpHeroku = () => {
   request.open("GET", "https://mq-chen-design-react.herokuapp.com/");
-  console.debug(new Date().toISOString() + ": Waking up Heroku App");
+  console.warn(new Date().toISOString() + ": Waking up Heroku App");
   request.send();
 }
 
-setInterval(WakeUpHeroku, 60*1000);
+setInterval(WakeUpHeroku, 15*60*1000);
