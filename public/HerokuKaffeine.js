699cbe34a5fc2da2d541572ffab117fce9bce6ab
jchen1379 <jianhuichen1379@gmail.com>
2021-09-08 11:44:40 -0400

    set HerokuKaffeine to 15 mins

--- public/HerokuKaffeine.js
+++ public/HerokuKaffeine.js
@@ -2,7 +2,6 @@ let request = new XMLHttpRequest();
 
 const WakeUpHeroku = () => {
   request.open("GET", "https://mq-chen-design-react.herokuapp.com/");
-  console.warn(new Date().toISOString() + ": Waking up Heroku App");
   request.send();
 }
 
