39ca0b4e578a1d54e7ad3c08cc00254eff45166f
jchen1379 <jianhuichen1379@gmail.com>
2023-11-05 00:20:46 -0400

    Update package-lock.json and minor cleanup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import reportWebVitals from './reportWebVitals';
 import {NavBar} from "./Components/Navigation/NavBar";
 import {ContentDisplayArea} from "./Components/Layout/ContentDisplayArea";
 import {Footer} from "./Components/Navigation/Footer";
@@ -16,8 +15,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
